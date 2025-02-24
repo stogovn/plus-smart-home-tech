0d7bcb0076fbf5060e2842b6cc2d069aa75be623
nikita.stogov <stogovn@yandex.ru>
2025-02-24 14:49:48 +0300

    fix: проверки v.8

--- telemetry/collector/src/main/java/ru/yandex/practicum/model/hub/ScenarioAddedEvent.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/model/hub/ScenarioAddedEvent.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.model.hub;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,9 +16,9 @@ public class ScenarioAddedEvent extends HubEvent {
     @NotBlank
     @Size(min = 3)
     private String name; // Название сценария
-
+    @NotNull
     private List<ScenarioCondition> conditions;
-
+    @NotNull
     private List<DeviceAction> actions;
 
     @Override
