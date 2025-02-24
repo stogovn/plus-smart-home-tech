69414fbce7f3267938985d6e2863fbaba2433e18
nikita.stogov <stogovn@yandex.ru>
2025-02-24 13:42:50 +0300

    fix: проверки v.7

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/AvroSerializer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/AvroSerializer.java
@@ -7,39 +7,28 @@ import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.HexFormat;
 
 public class AvroSerializer implements Serializer<SpecificRecordBase> {
-    private static final Logger log = LoggerFactory.getLogger(AvroSerializer.class);
-    private static final HexFormat hexFormat = HexFormat.ofDelimiter(":");
+    private final EncoderFactory encoderFactory = EncoderFactory.get();
+    private BinaryEncoder encoder;
 
     @Override
     public byte[] serialize(String topic, SpecificRecordBase data) {
-        log.info("==> serialize into topic = {}, data = {}", topic, data);
-
-        if (data == null) {
-            return null;
-        }
-
-        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-            DatumWriter<SpecificRecordBase> datumWriter = new SpecificDatumWriter<>(data.getSchema());
-            BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
-
-            datumWriter.write(data, encoder);
-
-            encoder.flush();
-
-            byte[] bytes = outputStream.toByteArray();
-            log.info("<== serialized in Avro:\n{}", hexFormat.formatHex(bytes));
-
-            return bytes;
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            byte[] result = null;
+            encoder = encoderFactory.binaryEncoder(out, null);
+            if (data != null) {
+                DatumWriter<SpecificRecordBase> writer = new SpecificDatumWriter<>(data.getSchema());
+                writer.write(data, encoder);
+                encoder.flush();
+                result = out.toByteArray();
+            }
+            return result;
         } catch (IOException e) {
-            throw new SerializationException("Serialization error", e);
+            throw new SerializationException("Serialization error for topic " + topic, e);
         }
     }
 }
