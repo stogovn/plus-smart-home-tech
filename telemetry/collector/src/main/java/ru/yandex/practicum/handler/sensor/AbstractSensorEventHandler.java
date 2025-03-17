8b24a8cccee5b2e8a1cf20939515975e945a2c9e
nikita.stogov <stogovn@yandex.ru>
2025-03-17 22:53:06 +0300

    fix: make the broker accessible v.6

--- telemetry/collector/src/main/java/ru/yandex/practicum/handler/sensor/AbstractSensorEventHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/handler/sensor/AbstractSensorEventHandler.java
@@ -1,31 +1,20 @@
 package ru.yandex.practicum.handler.sensor;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.retry.support.RetryTemplate;
 import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 
 @Slf4j
+@RequiredArgsConstructor
 public abstract class AbstractSensorEventHandler implements SensorEventHandler {
 
     protected final KafkaTemplate<String, Object> sensorKafkaTemplate;
-    private final RetryTemplate retryTemplate;
 
     @Value("${sensorEventTopic}")
     protected String topic;
-
-    // Конструктор с retryTemplate
-    public AbstractSensorEventHandler(KafkaTemplate<String, Object> sensorKafkaTemplate, RetryTemplate retryTemplate) {
-        this.sensorKafkaTemplate = sensorKafkaTemplate;
-        this.retryTemplate = retryTemplate;
-    }
-
-    // Перегруженный конструктор без retryTemplate
-    public AbstractSensorEventHandler(KafkaTemplate<String, Object> sensorKafkaTemplate) {
-        this(sensorKafkaTemplate, null);
-    }
 
     /**
      * Преобразует SensorEventProto в Avro-объект SensorEventAvro.
@@ -37,11 +26,7 @@ public abstract class AbstractSensorEventHandler implements SensorEventHandler {
         SensorEventAvro avroEvent = mapToAvro(eventProto);
         log.info("Отправляю Avro-событие в Kafka: {}", avroEvent);
         try {
-            retryTemplate.execute(context -> {
-                // Используем get() для синхронного ожидания завершения отправки
-                sensorKafkaTemplate.send(topic, avroEvent.getHubId(), avroEvent).get();
-                return null;
-            });
+            sensorKafkaTemplate.send(topic, avroEvent.getHubId(), avroEvent).get();
         } catch (Exception e) {
             log.error("Ошибка при отправке в Kafka", e);
         }
